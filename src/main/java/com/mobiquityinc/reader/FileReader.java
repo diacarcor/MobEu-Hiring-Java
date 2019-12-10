d20995e86b31c289c34814197f2d6ae49678a3f2
ing.diego.junco <ing.diego.junco@gmail.com>
2019-12-10 00:05:19 -0500

    Read file TDD Green phase. FileReader class implemented, now the test pass

--- src/main/java/com/mobiquityinc/reader/FileReader.java
+++ src/main/java/com/mobiquityinc/reader/FileReader.java
@@ -1,11 +1,32 @@
 package com.mobiquityinc.reader;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileReader {
 
+  private static final Logger LOG = Logger.getLogger("FileReader");
+
+  /**
+   * Reads a file from a given path and return contents as List of String
+   *
+   * @param path the file path.
+   * @return file contents as List<String>
+   */
   public static List<String> readFile(String path) {
+    try (Stream<String> stream = Files.lines(Paths.get(path))) {
+      LOG.log(Level.INFO, "File read correctly");
+      return stream.collect(Collectors.toList());
+    } catch (IOException ioe) {
+      LOG.log(Level.SEVERE, ioe.getMessage());
+    }
     return new ArrayList<>();
   }
 }
