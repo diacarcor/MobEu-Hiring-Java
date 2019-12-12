e38cd9a48c8ca8e0b9f1809e321162201c2a0663
Diego Junco <ing.diego.junco@gmail.com>
2019-12-11 20:23:37 -0500

    Lines max items validation. TDD Red phase Unit test implemented

--- src/test/java/com/mobiquityinc/parser/InputRecordParserTest.java
+++ src/test/java/com/mobiquityinc/parser/InputRecordParserTest.java
@@ -51,13 +51,24 @@ public class InputRecordParserTest {
     assertThrows(APIException.class, () -> InputRecordParser.parseFile(linesList));
   }
 
-  /** Checks if a line surpassing a 100 weight throws an APIException. */
+  /** Checks if a line surpassies a 100 weight throws an APIException. */
   @Test
   public void parseFile_packageMaxWeightSurpassed() {
     List<String> linesList = new ArrayList<>();
     linesList.add(
         "75 : (1,85.31,€29) (2,14.55,€74) (3,3.98,€16) (4,26.24,€55) (5,63.69,€52) (6,76.25,€75) (7,60.02,€74) (8,93.18,€35) (9,89.95,€78)");
     linesList.add("201 : (1,90.72,€13) (2,33.80,€40) (3,43.15,€10) (4,37.97,€16)");
+    assertThrows(APIException.class, () -> InputRecordParser.parseFile(linesList));
+  }
+
+  /** Checks if a line surpassing a 15 items throws an APIException. */
+  @Test
+  public void parseFile_lineMaxItemsSurpassed() {
+    List<String> linesList = new ArrayList<>();
+    linesList.add(
+        "75 : (1,85.31,€29) (2,14.55,€74) (3,3.98,€16) (4,26.24,€55) (5,63.69,€52) (6,76.25,€75) (7,60.02,€74) (8,93.18,€35) (9,89.95,€78)");
+    linesList.add(
+        "20 : (1,90.72,€13) (2,33.80,€40) (3,43.15,€10) (4,37.97,€16) (5,14.55,€74) (6,90.72,€13) (7,33.80,€40) (8,43.15,€10) (9,37.97,€16) (10,14.55,€74) (11,90.72,€13) (12,33.80,€40) (13,43.15,€10) (14,37.97,€16) (15,14.55,€74) (16,43.15,€10)");
     assertThrows(APIException.class, () -> InputRecordParser.parseFile(linesList));
   }
 }
